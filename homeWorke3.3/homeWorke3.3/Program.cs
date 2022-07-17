6f77f5029ede67ad71fdc81f5dd374b22a905a7e
Kyralesova <109028438+Kyralesova@users.noreply.github.com>
2022-07-17 20:08:01 +0300

    Add files via upload

---
+++ homeWorke3.3/homeWorke3.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace homeWorke3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Введите число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            bool primeNumber = true;
+            int i = 2;
+
+            while (i <= n / 2)
+            {
+                if (n % i == 0)
+                {
+                    primeNumber = false;
+                    break;
+                }
+                i++;
+            }
+            if (primeNumber)
+            {
+                Console.WriteLine("Число является простым");
+            }
+            else
+            {
+                Console.WriteLine("Число не является простым");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
+
