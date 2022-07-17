afa1fb8876fd7e2595897b64db9afa8a3618bef1
Kyralesova <109028438+Kyralesova@users.noreply.github.com>
2022-07-17 20:07:38 +0300

    Add files via upload

---
+++ homeWorke3.2/homeWorke3.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace homeWorke3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Приветствую, Вас!!! Введите число карт: ");
+            int cards = int.Parse(Console.ReadLine());
+            int sumCards = 0;
+
+            for (int i = 1; i <= cards; i++)
+            {
+                Console.Write("Введите номинал карты: ");
+                string nominal = Console.ReadLine();
+
+                int value = 0;
+
+                switch (nominal)
+                {
+                    case "J":
+                    case "Q":
+                    case "K":
+                    case "T":
+                        value = 10;
+                        break;
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                    case "10":
+                        value = int.Parse(nominal);
+                        break;
+                    default:
+                        Console.WriteLine("Допустимы номиналы 2-10, J, Q, K, T");
+                        i--;
+                        break;
+                }
+                sumCards += value;
+            }
+            Console.WriteLine("Сумма карт руки: " + sumCards);
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+
+}
---
+++ homeWorke3.2/homeWorke3.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("homeWorke3.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("homeWorke3.2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("227b8f1e-28af-433c-bd08-47a834d2abcd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
