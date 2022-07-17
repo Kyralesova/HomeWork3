c66bac4443a5a051873520c76f5dd6e87fba1d54
Kyralesova <109028438+Kyralesova@users.noreply.github.com>
2022-07-17 20:07:07 +0300

    Add files via upload

---
+++ homeWorke3.1/homeWorke3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homeWorke3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 != 0)
+            {
+                Console.WriteLine("Данное число не четное");
+            }
+            else
+            {
+                Console.WriteLine("Данное число четное");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
